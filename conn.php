33b6a968fda8ea41dedf1e74aed46d70a21c0d46
ShinAquila <augusto08rivera12@gmail.com>
2024-04-12 13:34:10 +0800

    first commit

---
+++ conn.php
@@ -0,0 +1,9 @@
+<?php
+    $connect = mysqli_connect('localhost','root','','db_android');
+
+    // if($connect) {
+    //     echo "Connected to database";
+    // } else {
+    //     echo "failed to connect to database";
+    // }
+?>
